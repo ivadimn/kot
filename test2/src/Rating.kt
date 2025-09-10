e175c758815738b8487190275b57e55a1553df60
vadim <ivadimn@mail.ru>
2025-09-10 20:24:22 +0300

    create test2

---
+++ test2/src/Rating.kt
@@ -0,0 +1,8 @@
+fun main() {
+    print("Введите оценку от 1 до 5: ")
+    val rating = readln()
+    print("Добавьте комментарий: ")
+    val comment = readln()
+
+    println("Спасибо за Вашу оценку!\nВы поставили: $rating\nВы оставили комментарий: $comment")
+}
