f601ae75f356e95cfb9848e2f8be3f42ddd17e8b
vadim <ivadimn@mail.ru>
2025-12-21 20:35:51 +0300

    new project

---
+++ Collections/src/main/kotlin/calculator/Test.kt
@@ -0,0 +1,9 @@
+package calculator
+
+fun main() {
+    val calc = Calculator()
+    val result  = calc.sum(10, 5)
+    println(result)
+    val result2  = calc.sum(100, 50)
+    println(result2)
+}
