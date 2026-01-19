92b03c1fa168e8f3456fc1a5fd3c7d8f231b67e2
vadim <ivadimn@mail.ru>
2026-01-19 17:38:09 +0300

    new project GUI

---
+++ Coroutines/src/main/kotlin/callbacks/Display.kt
@@ -0,0 +1,34 @@
+package org.example.callbacks
+
+import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
+
+object Display {
+    private val infoArea = JTextArea().apply {
+        isEditable = false
+    }
+    private val loadButton = JButton("Load Book")
+    private val timerLabel = JLabel("Time: 00:00")
+    private val topPanel = JPanel(BorderLayout()).apply {
+        add(timerLabel, BorderLayout.WEST)
+        add(loadButton, BorderLayout.EAST)
+    }
+
+    private val mainFrame = JFrame("Book and Author info").apply {
+        layout = BorderLayout()
+        add(topPanel, BorderLayout.NORTH)
+        add(JScrollPane(infoArea), BorderLayout.CENTER)
+        size = Dimension(800, 600)
+    }
+
+    fun show() {
+        mainFrame.isVisible = true
+    }
+
+}
